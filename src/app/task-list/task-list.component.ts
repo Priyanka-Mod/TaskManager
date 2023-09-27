c00df3ba1317fb547292974683c992f5c6671139
Priyanka Kevit <priyanka_mod@kevit.io>
2023-09-27 11:09:19 +0530

    Changed styling on input field

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { TaskService } from '../task.service';
 import { Task } from 'src/shared/model/task.model';
 
@@ -8,38 +8,59 @@ import { Task } from 'src/shared/model/task.model';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit{
-  savedTask:Array<Task>=[
-    {name:'Task 1' , isEditing :false, hover: false},
-    {name:'Task 2' , isEditing :false, hover: false},
-    {name:'Task 3' , isEditing :false, hover: false},
-    {name:'Task 4' , isEditing :false, hover: false},
-    {name:'Task 5' , isEditing :false, hover: false},
-    {name:'Task 6' , isEditing :false, hover: false},
-    {name:'Task 7' , isEditing :false, hover: false},
-    {name:'Task 8' , isEditing :false, hover: false},
-    {name:'Task 9' , isEditing :false, hover: false},
-    {name:'Task 10' , isEditing :false, hover: false}];
-  constructor(private taskService:TaskService){
+  @ViewChild('taskRef', {static:true}) elementRef: ElementRef;
+  savedTask: Array<Task> = [
+    { name: 'Task 1', isEditing: false, hover: false },
+    { name: 'Task 2', isEditing: false, hover: false },
+    { name: 'Task 3', isEditing: false, hover: false },
+    { name: 'Task 4', isEditing: false, hover: false },
+    { name: 'Task 5', isEditing: false, hover: false },
+    { name: 'Task 6', isEditing: false, hover: false },
+    { name: 'Task 7', isEditing: false, hover: false },
+    { name: 'Task 8', isEditing: false, hover: false },
+    { name: 'Task 9', isEditing: false, hover: false },
+    { name: 'Task 10', isEditing: false, hover: false }];
+  constructor(private taskService: TaskService) {
 
   }
+  // ngAfterViewInit(): void {
+  //   console.log(this.elementRef,'ngAfterViewInit');
+  // }
   ngOnInit(): void {
+    // console.log(this.elementRef,'ngOnInit');
     this.taskService.newTask.subscribe(
-      (response) => this.savedTask.push({name:response,isEditing:false, hover: false})
-    )}
+      (response) => this.savedTask.push({ name: response, isEditing: false, hover: false })
+    )
+  }
 
-  onEdit(selectedTask: Task):void {
-    this.savedTask.forEach((task)=>{
-        task.isEditing = false;
+  // ngAfterViewInit() {
+  //   this.elementRef.nativeElement.focus();
+  // }
+  
+  onEdit(selectedTask: Task): void {
+    this.savedTask.forEach((task) => {
+      task.isEditing = false;
     })
-    selectedTask.isEditing=true;   
+    selectedTask.isEditing = true;
+    setTimeout(()=>{
+         console.log(document.getElementById("inputTask"))
+         document.getElementById("inputTask").focus();
+    },0)
+    // console.log(this.elementRef,'onEdit');
   }
 
-  onDelete(selctedTask: Task):void{
-    this.savedTask =  this.savedTask.filter((task)=> task !== selctedTask)
+  onDelete(selctedTask: Task): void {
+    if(confirm("Do you want to delete the task?")){
+      this.savedTask = this.savedTask.filter((task) => task !== selctedTask)
+    }
+    else{
+      return
+    }
+    
   }
 
 
-  edited(selectedTask: Task):void {
+  edited(selectedTask: Task): void {
     selectedTask.isEditing = false;
   }
 }
